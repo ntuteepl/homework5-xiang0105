e5230bc5a4e2365a061782e0e858b141ee0b4edc
陳翊翔 <110757121+xiang0105@users.noreply.github.com>
2023-10-31 14:50:20 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    
+    float c ; 
+    double f;
+    
+    scanf("%f" , &c);
+    
+    f = c * 9/5 + 32 ; 
+
+    printf("%.1f" , f);  
+    
+}
