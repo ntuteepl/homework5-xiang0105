9f88fddc0c97d2020fd76acc60c255442652aff1
陳翊翔 <110757121+xiang0105@users.noreply.github.com>
2023-10-31 14:51:17 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    float h , wage , total;
+    
+    scanf("%f %f" , &h , &wage);
+    
+    if(h <= 60){
+        total *= h;
+    }else if(h <= 120 && h > 60){
+        total = (wage * 60) + (wage * 1.33 * (h - 60));
+    }else{
+        total = (wage * 60) + (wage * 1.33 * 60 ) + (wage * 1.66 * (h - 120));
+    }
+    
+    printf("%.1f" , total);
+    
+}
