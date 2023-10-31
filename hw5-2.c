e4b50e2cf71b5a6f3e90149fc7e969ff54e20edd
陳翊翔 <110757121+xiang0105@users.noreply.github.com>
2023-10-31 14:50:46 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float inp , total;
+    
+    scanf("%f" , &inp);
+    total = inp * 0.9;
+    
+    if(inp >= 1500){
+        total *= 0.79;
+    }else if(inp < 1500 && inp > 800){
+        total *= 0.9;
+    }
+    
+    printf("%.1f" , total);
+    
+}
